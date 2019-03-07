05e3eeab59b0aa02ff6a2943d3f14c0fdc8e296e
Perplexity <ryan4965@hotmail.co.uk>
2019-03-07 10:03:52 +0000

    Initial commit

---
+++ Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadWorker
+{
+ public static class Extensions
+    {
+        public static IEnumerable<List<T>> Split<T>(this List<T> locations, int nSize)
+        {
+            for (var i = 0; i < locations.Count; i += nSize)
+            {
+                yield return locations.GetRange(i, Math.Min(nSize, locations.Count - i));
+            }
+        }
+    }
+}
---
+++ ThreadWorker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadWorker
+{
+    public class ThreadWorker<T>
+    {
+        private List<Thread> WorkerThreads { get; }
+
+        public ThreadWorker(Action<T> job, List<T> collection, int threads)
+        {
+            WorkerThreads = new List<Thread>();
+            if (threads > collection.Count)
+            {
+                threads = collection.Count;
+            }
+            var remainder = collection.Count % threads;
+            var itemsPerThread = (collection.Count - remainder) / threads;
+            var splitCollection = collection.Split(itemsPerThread);
+            foreach (var coll in splitCollection)
+            {
+                var thread = new Thread(() =>
+                {
+                    foreach (var item in coll)
+                    {
+                        job.Invoke(item);
+                    }
+                });
+                WorkerThreads.Add(thread);
+            }
+        }
+
+        public void Start()
+        {
+            foreach (var thread in WorkerThreads)
+            {
+                thread.Start();
+            }
+        }
+    }
+}
